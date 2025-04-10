2cfef731531847d4ff21172a02adcc03e2a34d7e
Abijoe2004 <abijoe202004@gmail.com>
2025-04-11 00:25:36 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ViewnotificationsComponent } from './viewnotifications/viewnotifications.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ViewnotificationsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/viewnotifications/viewnotifications.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewnotifications',
+  imports: [],
+  templateUrl: './viewnotifications.component.html',
+  styleUrl: './viewnotifications.component.css'
+})
+export class ViewnotificationsComponent {
+
+}
